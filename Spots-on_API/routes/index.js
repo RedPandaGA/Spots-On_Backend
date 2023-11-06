27702a9c3f9afc478f067b4faa2f0e842a13ab0d
CulltheCake <110883605+CulltheCake@users.noreply.github.com>
2023-11-05 21:45:56 -0600

    login stuff

--- Spots-on_API/routes/index.js
+++ Spots-on_API/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/api/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the username exists in your database and fetch the stored hashed password.
+  // Compare the hashed password with the provided password using bcrypt.
+  // If they match, the login is successful.
+
+  if (usernameExists && bcrypt.compare(password, storedHashedPassword)) {
+    // Successful login
+    res.status(200).json({ message: 'Login successful' });
+  } else {
+    // Invalid login
+    res.status(401).json({ message: 'Invalid login credentials' });
+  }
 });
 
 module.exports = router;
