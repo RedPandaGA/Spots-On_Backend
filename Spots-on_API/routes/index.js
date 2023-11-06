06257186633d29f999d95b82683eee07e7b61d99
CulltheCake <110883605+CulltheCake@users.noreply.github.com>
2023-11-06 10:53:22 -0600

    store event creation(locally)

--- Spots-on_API/routes/index.js
+++ Spots-on_API/routes/index.js
@@ -1,21 +1,78 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-router.post('/api/login', async (req, res) => {
+
+// In-memory storage for user data (replace with a real database)
+const users = [
+  {
+    username: 'testuser',
+    password: 'password123', // Store passwords securely in production
+  },
+  // Add more user objects here
+];
+
+let authenticatedUser = null; // Temporary storage for authenticated user (not recommended for production)
+
+app.use(bodyParser.json());
+
+app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
 
-  // Check if the username exists in your database and fetch the stored hashed password.
-  // Compare the hashed password with the provided password using bcrypt.
-  // If they match, the login is successful.
+  // Simulate user authentication
+  const user = users.find((user) => user.username === username && user.password === password);
 
-  if (usernameExists && bcrypt.compare(password, storedHashedPassword)) {
-    // Successful login
+  if (user) {
+    // Store the authenticated user in a local variable (for testing only)
+    authenticatedUser = user;
+
     res.status(200).json({ message: 'Login successful' });
   } else {
-    // Invalid login
     res.status(401).json({ message: 'Invalid login credentials' });
   }
 });
+
+app.get('/api/user-data', (req, res) => {
+  if (authenticatedUser) {
+    res.status(200).json({ username: authenticatedUser.username });
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+// Middleware for parsing JSON data
+app.use(bodyParser.json());
+
+// Initialize an empty variable to store the event data
+let events = [];
+
+// Define a route to handle the POST request to store an event
+app.post('/api/events', (req, res) => {
+  const event = req.body;
+
+  // Add the received event data to the events array
+  events.push(event);
+
+  res.status(201).json({ message: 'Event created successfully' });
+});
+
+// Define a route to retrieve all stored events
+app.get('/api/events', (req, res) => {
+  res.json(events);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 
 module.exports = router;
