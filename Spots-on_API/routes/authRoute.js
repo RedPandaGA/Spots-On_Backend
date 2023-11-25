105b976f0e8c1cfaea419551782fdc8a161b6dcb
CulltheCake <110883605+CulltheCake@users.noreply.github.com>
2023-11-24 18:17:24 -0600

    Revert "incog fetch"
    This reverts commit 2b3f137b75d1d290c44a7410831dc09413928ec7.

--- Spots-on_API/routes/authRoute.js
+++ Spots-on_API/routes/authRoute.js
@@ -210,14 +210,13 @@ authRouter.post('/updateUserLocation', async (req, res) => {
 
 authRouter.post('/updateIncog', async (req, res) => {
     const { uid,  } = req.body;
-    try {   
-        console.log({incognito});
-        const client = await pool.connect();
-        const result = await client.query(
-            `UPDATE user_data SET incognito = '${incognito}' WHERE uid = '${uid}'`);
-         const dbres = result;
+    try {
+        // console.log(location);
+        // const client = await pool.connect();
+        // const result = await client.query(`UPDATE user_data SET loc_history = array_prepend('${JSON.stringify(location)}', loc_history) WHERE uid='${uid}'`);
+        // const dbres = result;
         dbres = { success: "Updated Incog"}
-         client.release();
+        // client.release();
 
         res.status(200).json(dbres);
     } catch (err) {
